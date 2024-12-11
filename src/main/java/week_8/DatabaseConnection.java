8d94eda9e31d93b32ba4a3b35ad5637de5710297
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-10 19:42:49 -0600

    Auto-commit Tue Dec 10 19:42:49 CST 2024

--- src/main/java/week_8/DatabaseConnection.java
+++ src/main/java/week_8/DatabaseConnection.java
@@ -3,14 +3,28 @@ package week_8;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/mysql_db"; // Database name is 'mysql_db'
-    private static final String USER = "root";
-    private static final String PASSWORD = "your_password";  // Replace with your actual password
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql_db"; // Correct database name
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        Properties properties = new Properties();
+        properties.put("user", "root");
+        properties.put("password", "Katmandu7$");
+        properties.put("useSSL", "false");
+        properties.put("autoReconnect", "true");
+
+        return DriverManager.getConnection(URL, properties);
     }
 }
+
 
