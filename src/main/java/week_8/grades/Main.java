6aed6779aaf74d2d69838e7082b551e0a9db73d4
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-10 20:56:09 -0600

    Auto-commit Tue Dec 10 20:56:09 CST 2024

--- src/main/java/week_8/grades/Main.java
+++ src/main/java/week_8/grades/Main.java
@@ -1,16 +1,12 @@
-package week_8.grades;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseConnection.initializeDatabase(); // Ensure this method creates the table
-
         Scanner scanner = new Scanner(System.in);
+        StudentDAO studentDao = new StudentDAO();  // Create an instance of StudentDAO
+        studentDao.createTable();  // Ensure the table is created before any operations are performed
+
         boolean running = true;
-
-        CreateTable.createNewTable(); // Method that creates the table if it doesn't exist
-
         while (running) {
             System.out.println("\nChoose an operation:");
             System.out.println("1 - Insert new student");
@@ -18,10 +14,9 @@ public class Main {
             System.out.println("3 - Update a student's grade");
             System.out.println("4 - List all students");
             System.out.println("5 - Exit");
-
             System.out.print("Enter option: ");
-            int option = scanner.nextInt();
 
+            int option = scanner.nextInt();
             switch (option) {
                 case 1:
                     System.out.print("Enter name: ");
@@ -30,22 +25,22 @@ public class Main {
                     int age = scanner.nextInt();
                     System.out.print("Enter grade: ");
                     double grade = scanner.nextDouble();
-                    InsertData.insert(name, age, grade);
+                    studentDao.insertStudent(name, age, grade);
                     break;
                 case 2:
                     System.out.print("Enter student ID to delete: ");
                     int id = scanner.nextInt();
-                    DeleteData.delete(id);
+                    studentDao.deleteStudent(id);
                     break;
                 case 3:
                     System.out.print("Enter student ID to update: ");
                     int updateId = scanner.nextInt();
                     System.out.print("Enter new grade: ");
                     double newGrade = scanner.nextDouble();
-                    UpdateData.update(updateId, newGrade);
+                    studentDao.updateStudentGrade(updateId, newGrade);
                     break;
                 case 4:
-                    RetrieveData.selectAll();
+                    studentDao.listAllStudents();
                     break;
                 case 5:
                     System.out.println("Exiting...");
@@ -59,5 +54,3 @@ public class Main {
         scanner.close();
     }
 }
-
-
