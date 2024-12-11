b5dac7db865478bfc8dde978a78d3c6b81ad5263
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-10 19:41:47 -0600

    Auto-commit Tue Dec 10 19:41:47 CST 2024

--- src/main/java/week_8/DatabaseConnection.java
+++ src/main/java/week_8/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/mysql_db"; // Make sure 'my_database' is your database name
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql_db"; // Database name is 'mysql_db'
     private static final String USER = "root";
     private static final String PASSWORD = "your_password";  // Replace with your actual password
 
