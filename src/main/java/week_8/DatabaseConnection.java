0eb1c2514ac06dab505161506b73b392a465ace9
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-10 20:18:27 -0600

    Auto-commit Tue Dec 10 20:18:27 CST 2024

--- src/main/java/week_8/DatabaseConnection.java
+++ src/main/java/week_8/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/hellos.db";
+    private static final String URL = "jdbc:sqlite://localhost:3306/hellos.db";
 
     static {
         try {
