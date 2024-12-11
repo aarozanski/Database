20b9d9d06cc4a7d7cbc1d6b1a53d61fcacf62bb8
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-10 20:28:38 -0600

    Auto-commit Tue Dec 10 20:28:38 CST 2024

--- src/main/java/week_8/DatabaseConnection.java
+++ src/main/java/week_8/DatabaseConnection.java
@@ -3,26 +3,18 @@ package week_8;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlite://localhost:3306/hellos.db";
+    private static final String URL = "jdbc:sqlite:path_to_your_database.db";
 
-    static {
+    public static Connection connect() {
+        Connection conn = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        Properties properties = new Properties();
-        properties.put("user", "root");
-        properties.put("password", "Katmandu7$"); // Make sure this password meets your MySQL configuration's requirements
-        properties.put("useSSL", "false");
-        properties.put("autoReconnect", "true");
-
-        return DriverManager.getConnection(URL, properties);
+        return conn;
     }
 }
