63fba16806df1466ef799d234741ab906f166799
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-16 00:16:33 -0600

    Auto-commit Mon Dec 16 00:16:33 CST 2024

--- src/main/java/week_10/LibrarySimulation.java
+++ src/main/java/week_10/LibrarySimulation.java
@@ -3,24 +3,22 @@ package week_10;
 public class LibrarySimulation {
     public static void main(String[] args) throws InterruptedException {
         Library library = new Library();
-        // Populate the library with books
-        library.addBook(new Book(1, "1984", "George Orwell"));
-        library.addBook(new Book(2, "The Great Gatsby", "F. Scott Fitzgerald"));
+        library.addBook(new Book(1, "Java Fundamentals", "John Doe"));
+        library.addBook(new Book(2, "Data Structures", "Jane Roe"));
 
-        // Create user threads
         Thread[] users = new Thread[5];
         for (int i = 0; i < users.length; i++) {
-            users[i] = new Thread(new User(library));
+            users[i] = new Thread(new User(library, i+1));
+            users[i].setPriority(Thread.MIN_PRIORITY + i % (Thread.MAX_PRIORITY - Thread.MIN_PRIORITY));
             users[i].start();
         }
 
-        // Wait for all threads to complete
         for (Thread user : users) {
             user.join();
         }
 
-        // Display final list of available books
         library.displayAvailableBooks();
     }
 }
+
 
