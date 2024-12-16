836dc857f0e2b45268919737009c9d012173b3e7
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-16 13:22:55 -0600

    Auto-commit Mon Dec 16 13:22:55 CST 2024

--- src/main/java/week_1/calculator/Calculator.java
+++ src/main/java/week_1/calculator/Calculator.java
@@ -1,4 +1,10 @@
 package week_1.calculator;
 
 public class Calculator {
+    public static int main(String[] args) {
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+    }
 }
