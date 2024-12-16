2f606a6d99f7cbc2826627464d0f400f341585c5
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-16 12:31:10 -0600

    Auto-commit Mon Dec 16 12:31:10 CST 2024

--- src/test/java/week_10/BookTest.java
+++ src/test/java/week_10/BookTest.java
@@ -1,4 +1,38 @@
 package week_10;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BookTest {
+    private Book book;
+
+    @BeforeEach
+    void setUp() {
+        book = new Book(1, "Effective Java", "Joshua Bloch");
+    }
+
+    @Test
+    void testBookCheckout() {
+        assertTrue(book.isAvailable(), "Book should be available initially");
+        book.checkOut("User 1");
+        assertFalse(book.isAvailable(), "Book should not be available after being checked out");
+    }
+
+    @Test
+    void testReturnBook() {
+        book.checkOut("User 1");
+        book.returnBook("User 1");
+        assertTrue(book.isAvailable(), "Book should be available after being returned");
+    }
+
+    @Test
+    void testAvailability() {
+        book.checkOut("User 1");
+        assertFalse(book.isAvailable(), "Book should report not available when checked out");
+        book.returnBook("User 1");
+        assertTrue(book.isAvailable(), "Book should report available after being returned");
+    }
 }
+
