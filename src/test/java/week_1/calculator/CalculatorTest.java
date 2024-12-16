339d0c8957c4b6612e5af7dbd78cb63a14b592ec
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-16 13:28:00 -0600

    Auto-commit Mon Dec 16 13:28:00 CST 2024

--- src/test/java/week_1/calculator/CalculatorTest.java
+++ src/test/java/week_1/calculator/CalculatorTest.java
@@ -1,4 +1,39 @@
 package week_1.calculator;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3), "Adding 2 and 3 should return 5");
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(2, 3), "Subtracting 3 from 2 should return -1");
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.multiply(2, 3), "Multiplying 2 and 3 should return 6");
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.divide(4, 2), "Dividing 4 by 2 should return 2");
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(5, 0),
+                "Dividing by zero should throw IllegalArgumentException");
+    }
 }
+
