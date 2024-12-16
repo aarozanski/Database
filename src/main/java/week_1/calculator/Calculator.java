cbd49f91772ca4782d09dda0383dfb724d68a7e2
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-16 13:23:56 -0600

    Auto-commit Mon Dec 16 13:23:56 CST 2024

--- src/main/java/week_1/calculator/Calculator.java
+++ src/main/java/week_1/calculator/Calculator.java
@@ -1,10 +1,27 @@
 package week_1.calculator;
 
 public class Calculator {
-    public static int main(String[] args) {
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-        public int add(int a, int b) {
-            return a + b;
+    // Method to subtract two integers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Method to multiply two integers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to divide two integers
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
         }
+        return a / b;
     }
 }
+
