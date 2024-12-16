93d24976ebdb596053b65b2532b354726a7eacb3
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-16 12:32:11 -0600

    Auto-commit Mon Dec 16 12:32:11 CST 2024

--- src/main/java/week_10/LibraryTest.java
+++
@@ -1,4 +0,0 @@
-package week_10;
-
-public class LibraryTest {
-}
---
+++ src/test/java/week_10/LibraryTest.java
@@ -0,0 +1,30 @@
+package week_10;
+
+public class LibraryTest {
+    private Library library;
+
+    @BeforeEach
+    void setUp() {
+        library = new Library();
+        library.addBook(new Book(1, "Effective Java", "Joshua Bloch"));
+    }
+
+    @Test
+    void testAddBook() {
+        assertEquals(1, library.size(), "Library should have one book after adding one book");
+    }
+
+    @Test
+    void testCheckOutBook() {
+        library.checkOutBook(0, "User 1");
+        assertFalse(library.isAvailable(0), "Book should not be available after checkout");
+    }
+
+    @Test
+    void testReturnBook() {
+        library.checkOutBook(0, "User 1");
+        library.returnBook(0, "User 1");
+        assertTrue(library.isAvailable(0), "Book should be available after being returned");
+    }
+}
+
