a8ef560999f1606d0a80a3375010c533a8a2ea02
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-19 16:23:53 -0600

    Auto-commit Thu Dec 19 16:23:53 CST 2024

--- src/main/java/week_11/StringReversal.java
+++ src/main/java/week_11/StringReversal.java
@@ -1,18 +1,19 @@
 package week_11;
 
 public class StringReversal {
-    public void reverseString(char[] s) {
-        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-        }
-    }
 
     public static void main(String[] args) {
         char[] testArray = {'h', 'e', 'l', 'l', 'o'};
         StringReversal sr = new StringReversal();
         sr.reverseString(testArray);
         System.out.println(testArray);
+    }
+
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
     }
 }
