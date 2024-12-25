30ffe03b53c5d697dc7d3fce400cad7c3b4013f2
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-25 12:46:16 -0600

    Auto-commit Wed Dec 25 12:46:16 CST 2024

--- src/main/java/week_11/StringReversal.java
+++ src/main/java/week_11/StringReversal.java
@@ -4,12 +4,11 @@ public class StringReversal {
 
     public static void main(String[] args) {
         char[] testArray = {'h', 'e', 'l', 'l', 'o'};
-        StringReversal sr = new StringReversal();
-        sr.reverseString(testArray);
+        reverseString(testArray);
         System.out.println(testArray);
     }
 
-    public void reverseString(char[] s) {
+    public static void reverseString(char[] s) {
         for (int i = 0, j = s.length - 1; i < j; i++, j--) {
             char temp = s[i];
             s[i] = s[j];
